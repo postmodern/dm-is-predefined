79e057e9999eb64085413308bfa8433535253f47
postmodern <postmodern.mod3@gmail.com>
2008-12-19 19:38:33 -0800

    Added spec tests.

--- spec/integration/predefined_spec.rb
+++ spec/integration/predefined_spec.rb
@@ -1,8 +1,69 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
 
-if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
+if (HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES)
   describe 'DataMapper::Predefined' do
+    before(:all) do
+      class TestModel
 
+        include DataMapper::Resource
+
+        is :predefined
+
+        # Name property to test String values
+        property :name, String
+
+        # Number property to test Integer values
+        property :number, Integer
+
+        # Optional property to test default values
+        property :optional, String, :default => 'hello'
+
+        # Body property to test Text values
+        property :body, Text
+
+        define :test1, :name => 'test1',
+                       :number => 1,
+                       :optional => 'yo',
+                       :body => %{This is a test.}
+
+        define :test2, :name => 'test2', :number => 2
+      end
+
+      TestModel.auto_migrate!
+    end
+
+    it "should it be able to define resources of a Model" do
+      test1 = TestModel.test1
+
+      test1.should_not be_nil
+      test1.name.should == 'test1'
+      test1.number.should == 1
+      test1.optional.should == 'yo'
+      test1.body.should == %{This is a test.}
+    end
+
+    it "should it be able to define resources with empty attributes" do
+      test2 = TestModel.test2
+
+      test2.should_not be_nil
+      test2.name.should == 'test2'
+      test2.number.should == 2
+      test2.optional.should == 'hello'
+      test2.body.should be_nil
+    end
+
+    it "should provide a generic interface for accessing resources" do
+      test1 = TestModel[:test1]
+
+      test1.name.should == 'test1'
+      test1.number.should == 1
+    end
+
+    it "should raise an UnknownResource exception when accessing undefined resources" do
+      lambda {
+        TestModel[:test3]
+      }.should raise_error(DataMapper::Predefined::UnknownResource)
+    end
   end
 end
