1a9dcb5b454dd0fabdc2928d0130b5fad9505f7a
postmodern <postmodern.mod3@gmail.com>
2010-05-20 19:01:44 -0700

    Do not include DataMapper::Predefined into Resource by default.

--- lib/dm-predefined.rb
+++ lib/dm-predefined.rb
@@ -8,13 +8,3 @@ require 'dm-core'
 
 # Require plugin-files
 require Pathname(__FILE__).dirname.expand_path / 'dm-predefined' / 'predefined.rb'
-
-
-# Include the plugin in Resource
-module DataMapper
-  module Resource
-    module ClassMethods
-      include DataMapper::Predefined
-    end # module ClassMethods
-  end # module Resource
-end # module DataMapper
